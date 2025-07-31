bb8473a80d01b5825ca49b4e61c0696843a6a487
ilhamhafizha <muhammadilhamhafizha@gmail.com>
2025-07-31 15:35:00 +0700

    feat: implement useState quantity product

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { IoIosRemove, IoIosAdd } from "react-icons/io";
+
 interface Props {
   imageUrl: string;
   name: string;
@@ -8,6 +12,21 @@ interface Props {
 export const ProductCard = (props: Props) => {
   const { imageUrl, name, price, stock } = props;
 
+  const [quantity, setquantity] = useState(0);
+
+  const addquantity = () => {
+    if (quantity < stock) {
+      setquantity(quantity + 1);
+    }
+  };
+  const removequantity = () => {
+    if (quantity > 0) setquantity(quantity - 1);
+  };
+
+  const addCart = () => {
+    alert("Add Cart");
+  };
+
   return (
     <div className="p-4 border rounded-md md:max-w-96 flex flex-col gap-4">
       <div className="aspect-square w-full overflow-hidden">
@@ -16,7 +35,25 @@ export const ProductCard = (props: Props) => {
       <div>
         <p className="text-lg">{name}</p>
         <p className="text-xl font-semibold">Rp {price.toLocaleString("id-ID")}</p>
-        <p className="text-muted-foreground text-sm">In stock: {stock}</p>
+        <p className="text-muted-foreground text-sm">Stock: {stock}</p>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        {/* Quantity */}
+        <div className="flex justify-between items-center">
+          <Button disabled={quantity <= 0} size="icon" variant="ghost" onClick={removequantity}>
+            <IoIosRemove className="w-6 h-6" />
+          </Button>
+          <p className="text-lg font-bold">{quantity}</p>
+          <Button disabled={quantity >= stock} size="icon" variant="ghost" onClick={addquantity}>
+            <IoIosAdd className="w-6 h-6" />
+          </Button>
+        </div>
+
+        {/* Add Cart */}
+        <Button className="w-full" onClick={addCart}>
+          Add Cart
+        </Button>
       </div>
     </div>
   );
