d6f3212041886bc70490bed4a316b28b9d4eb5f3
ilhamhafizha <muhammadilhamhafizha@gmail.com>
2025-08-07 14:00:43 +0700

    faet: adding a product table in admin page

--- src/pages/admin/ProductManagementPage.tsx
+++ src/pages/admin/ProductManagementPage.tsx
@@ -1,23 +1,83 @@
 import AdminLayout from "@/components/Layout/AdminLayout";
 import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { axiosIntance } from "@/lib/axios";
+import { Ellipsis } from "lucide-react";
+import { useEffect, useState } from "react";
 import { IoAdd } from "react-icons/io5";
 
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  stock: number;
+}
+
 const ProductManagementPage = () => {
+  const [product, setProducts] = useState<Product[]>([]);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await axiosIntance.get("/products", {
+        params: { _limit: 5 },
+      });
+      setProducts(response.data);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
-    <div>
-      <AdminLayout
-        title="Product Management"
-        description=" Manage your products, update details, and track inventory"
-        rightSection={
-          <Button>
-            <IoAdd className="h-6 w-6 mr-2" />
-            Add Product
-          </Button>
-        }
-      >
-        <h1>Product Management Page Content</h1>
-      </AdminLayout>
-    </div>
+    <AdminLayout
+      title="Product Management"
+      description=" Manage your products, update details, and track inventory"
+      rightSection={
+        <Button>
+          <IoAdd className="h-6 w-6 mr-2" />
+          Add Product
+        </Button>
+      }
+    >
+      <Table className="p-4 border rounded-md">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">ID</TableHead>
+            <TableHead>Product Name</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>Stock</TableHead>
+            <TableHead></TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {product.map((product) => {
+            return (
+              <TableRow>
+                <TableCell>{product.id}</TableCell>
+                <TableCell>{product.name}</TableCell>
+                <TableCell>Rp {product.price.toLocaleString("id-ID")}</TableCell>
+                <TableCell>{product.stock}</TableCell>
+                <TableCell>
+                  <Button variant="ghost" size={"icon"} className="ml-2">
+                    <Ellipsis className="w-6 h-6" />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </AdminLayout>
   );
 };
 
